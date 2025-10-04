cba474af3eb6e64b22d09e32df894286dfe8f3fd
Raphael2334 <johnraphaelreganit23@gmail.com>
2025-10-04 20:35:29 +0800

    Add files via upload
    navbar works and file runs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: "#eee", minHeight: "100vh" }}>
+      <Navbar />
+      <div className="container mt-5">
+        <h1>Hello, Cook Together!</h1>
+        <p>This is your React app running with Bootstrap.</p>
+      </div>
     </div>
   );
 }
