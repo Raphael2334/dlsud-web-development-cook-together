fb6f80fc4138351499df86aa8794cea8f9a23aca
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-10-05 12:30:20 +0800

    test: add basic template to Aside component

---
+++ src/components/Aside.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function Aside() {
+    return (
+      <Container>
+        <h1>Aside links</h1>
+      </Container>  
+    );
+} 
