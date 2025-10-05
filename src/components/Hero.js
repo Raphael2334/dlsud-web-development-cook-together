6138a52cb712efe3b4004c08a001a3599fab478d
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-10-05 12:27:45 +0800

    test: add basic template to Hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, Button } from 'react-bootstrap';
+
+function Hero() {
+    return (
+        <Container>
+            <h1>Hero Section</h1>
+        </Container>
+    )
+}
