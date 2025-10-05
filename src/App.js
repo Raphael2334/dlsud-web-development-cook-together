001bd5b673a0076a1cd2abdcb912712621ba8e51
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-10-05 12:40:42 +0800

    test: add basic routing config to App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar as NavbarComponent } from './components/Navbar';
+import { Hero as HeroComponent } from './components/Hero';
+import { Footer as FooterComponent } from './components/Footer';
+import Landing from './pages/Landing';
+import Registration from './pages/Registration';
+import Login from './pages/Login';
+import Recipes from '.pages/Recipes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavbarComponent />
+      <HeroComponent />
+      <Routes>
+        <Route path='/landing' element={Landing} />
+        <Route path='/registration' element={Registration} />
+        <Route path='/login' element={Login} />
+        <Route path='/home-recipes' element={Recipes} />
+      </Routes>
+      <FooterComponent />
+    </Router>
   );
 }
 
