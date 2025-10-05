5aff176fd235754468ca7c0ace8e38cec657e787
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-10-05 15:02:36 +0800

    fix: path to logo from NavbarComponent

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navbar, Nav, Container, Button, Dropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.png";
+import logo from "../assets/icons/logo.png";
 import "../styles/colors.css";
 
 function NavbarComponent({
