8d2247068bcd1856a51612905568dd869a65964e
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-10-05 12:33:09 +0800

    test: add basic template to Body component

---
+++ src/components/Body.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function Body() {
+    return(
+        <Container>
+            <h2>Body section</h2>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis vero velit porro, explicabo minus dolorum aperiam voluptatem sapiente, harum cum voluptas! Ratione, sunt error! Porro recusandae quod, quaerat mollitia ducimus cum vitae dignissimos cupiditate. Modi, animi nam aut et velit hic doloremque sequi cum in iusto deserunt voluptatum. Ut voluptatem aperiam praesentium asperiores delectus quae sunt, non, voluptas quos tempore sapiente excepturi cum. Vero praesentium incidunt quibusdam error doloribus cumque natus laboriosam! Nam quidem eveniet perspiciatis, quis similique dolor quasi tempore ad laboriosam, porro praesentium tenetur odio sit obcaecati modi? Tenetur corrupti minima nihil maxime, magni assumenda ducimus eum eveniet?</p>
+        </Container>
+    );
+}
+
+export default Body;
