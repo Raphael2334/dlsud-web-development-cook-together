322270f5d9181b8e888cc8fd99e53c467f211e1a
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-10-08 15:40:53 +0800

    style: rename api obj to apiLinks

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,6 +1,6 @@
-const api = {
+const apiLinks = {
     main: "https://sheetdb.io/api/v1/bry4xqkea1b8c",
     users: "https://sheetdb.io/api/v1/bry4xqkea1b8c?sheet=Users",
 }
 
-export default api;
+export default apiLinks;
